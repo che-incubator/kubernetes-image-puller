4ee03decb91648577a2be6308d7956c7e34d3468
Tom George <tgeorge@redhat.com>
2020-02-13 07:41:00 -0600

    Update version of git in test runner
    Signed-off-by: Tom George <tgeorge@redhat.com>

--- cico_functions.sh
+++ cico_functions.sh
@@ -25,7 +25,9 @@ function install_deps() {
 
   yum -y -d 1 update
   yum -y -d 1 install epel-release
-  yum -y -d 1 install --enablerepo=epel docker make golang git
+  yum -y -d 1 install --enablerepo=epel docker make golang
+  yum -y install  https://centos7.iuscommunity.org/ius-release.rpm
+  yum -y install  git2u-all
   systemctl start docker
 
   # Login to quay.io
