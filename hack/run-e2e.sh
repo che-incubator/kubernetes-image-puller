e3a219a92cc1b4c37b69dd74cb07de4e0abf1c66
nickboldt <nboldt@redhat.com>
2021-06-25 16:10:00 -0300

    chore: use :next instead of :nightly ( eclipse/che/issues/19291 )
    Change-Id: Ia1c826f1848bbf2f80c1a86469b6790329b36a0b
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- hack/run-e2e.sh
+++ hack/run-e2e.sh
@@ -9,7 +9,7 @@ if [[ $(kind get clusters | grep k8s-image-puller-e2e | wc -l) == '0' ]]; then
 fi
 kubectl config set current-context kind-k8s-image-puller-e2e
 export CACHING_INTERVAL_HOURS='1'
-export IMAGES='che-theia=quay.io/eclipse/che-theia:next;che-plugin-registry=quay.io/eclipse/che-plugin-registry:nightly'
+export IMAGES='che-theia=quay.io/eclipse/che-theia:next;che-plugin-registry=quay.io/eclipse/che-plugin-registry:next'
 go test -count=1 -v ./e2e... 
 
 if [[ $1 == "--rm" ]]; then
