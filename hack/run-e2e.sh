86411b4c842cfba9aeea67d54be41c7704f7dbec
David Kwon <dakwon@redhat.com>
2021-08-20 16:11:36 -0400

    chore(tests): Add e2e tests for pre-pulling different types of images
    Signed-off-by: David Kwon <dakwon@redhat.com>

--- hack/run-e2e.sh
+++ hack/run-e2e.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
 
-function cleanup() {
+export CACHING_INTERVAL_HOURS='1'
+export DEPLOYMENT_NAME='kubernetes-image-puller'
+export NAMESPACE='k8s-image-puller'
+export DAEMONSET_NAME='kubernetes-image-puller'
+
+function createClusterIfNeeded() {
+  if [[ $(kind get clusters | grep k8s-image-puller-e2e | wc -l) == '0' ]]; then 
+    kind create cluster --name k8s-image-puller-e2e 
+  fi
+}
+
+function setContext() {
+  kubectl config set current-context kind-k8s-image-puller-e2e
+}
+
+function setupCluster() {
+  kubectl create namespace k8s-image-puller
+  helm template -s templates/app.yaml ./deploy/helm | kubectl apply -n k8s-image-puller -f -
+}
+
+function cleanupCluster() {
+  kubectl delete namespace k8s-image-puller --wait=true
+}
+
+function deleteCuster() {
   kind delete cluster --name k8s-image-puller-e2e
 }
 
-if [[ $(kind get clusters | grep k8s-image-puller-e2e | wc -l) == '0' ]]; then 
-  kind create cluster --name k8s-image-puller-e2e 
-fi
-kubectl config set current-context kind-k8s-image-puller-e2e
-export CACHING_INTERVAL_HOURS='1'
-export IMAGES='che-theia=quay.io/eclipse/che-theia:next;che-plugin-registry=quay.io/eclipse/che-plugin-registry:next'
-go test -count=1 -v ./e2e... 
+createClusterIfNeeded
+setContext
+setupCluster
+go test -count=1 -v ./e2e...
+cleanupCluster
 
 if [[ $1 == "--rm" ]]; then
-  cleanup
+  deleteCuster
 fi
